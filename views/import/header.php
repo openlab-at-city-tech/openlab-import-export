7a3069224e949f57cbb0a30f5306566776920467
Boone B Gorges <boonebgorges@gmail.com>
2021-07-17 06:54:58 -0500

    Add 'before you begin' notice to Import panel.

--- views/import/header.php
+++ views/import/header.php
@@ -1,2 +1,3 @@
 <h1><?php esc_html_e( 'OpenLab Site Import', 'openlab-import-export' ); ?></h1>
 <p><?php esc_html_e( 'Use this tool to import previously exported OpenLab Site Archive.', 'openlab-import-export' ); ?></p>
+<p><?php echo wp_kses_post( __( '<strong>Before you begin:</strong> The OpenLab Archive contains a readme file with information about plugins you may want to activate before performing the import, as well as other information about the import. Please read through the file before beginning the import.', 'openlab-import-export' ) ); ?></p>
